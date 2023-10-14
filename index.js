ba9315119548d6300176dde8dafd15f2aafdbde5
Jambr0li <jasonkovalenko@gmail.com>
2023-10-14 12:59:59 -0700

    create background stars

--- index.js
+++ index.js
@@ -74,12 +74,13 @@ class Projectile {
 
 
 class Particle {
-    constructor({position, velocity, radius, color}){
+    constructor({position, velocity, radius, color, fades}){
         this.position = position
         this.velocity = velocity
         this.radius = radius
         this.color = color
         this.opacity = 1
+        this.fades = fades
     }
 
     draw() {
@@ -97,7 +98,7 @@ class Particle {
         this.draw()
         this.position.x += this.velocity.x
         this.position.y += this.velocity.y
-        this.opacity-= 0.01
+        if (this.fades) this.opacity-= 0.01
     }
 }
 
@@ -234,7 +235,22 @@ const keys = {
 let frames = 0
 let randomInterval = Math.floor((Math.random() * 500) + 500)
 
-function createParticles({object, color}) {
+for (let i = 0; i < 100; i++){
+    particles.push(new Particle({
+        position: {
+            x: Math.random() * canvas.width,
+            y: Math.random() * canvas.height,
+        },
+        velocity: {
+            x: 0,
+            y: .5
+        },
+        radius: Math.random() * 3,
+      color: 'white'
+    }))
+}
+
+function createParticles({object, color, fades}) {
     for (let i = 0; i < 15; i++){
         particles.push(new Particle({
             position: {
@@ -246,7 +262,8 @@ function createParticles({object, color}) {
                 y: (Math.random() - 0.5) * 2
             },
             radius: Math.random() * 3,
-          color: color || "#BAA0DE"
+          color: color || "#BAA0DE",
+          fades: fades
         }))
     }
 }
@@ -258,6 +275,12 @@ function animate(){
     player.update()
 
     particles.forEach((particle,index) => {
+
+        if (particle.position.y - particle.radius >= canvas.height) {
+            particle.position.x = Math.random() * canvas.width 
+            particle.position.y = -particle.radius
+        } 
+
         if (particle.opacity <= 0) {
             setTimeout(() => {
                 particles.splice(index, 1)
@@ -276,7 +299,8 @@ function animate(){
         if (invaderProjectile.position.y + invaderProjectile.height >= player.position.y && invaderProjectile.position.x + invaderProjectile.width >= player.position.x && invaderProjectile.position.x <= player.position.x + player.width){
             createParticles({
                 object: player,
-                color: "white"
+                color: "white",
+                fades: true
             })
             setTimeout(() => {
                 invaderProjectiles.splice(index, 1)
@@ -321,7 +345,8 @@ function animate(){
                         if (invaderFound && projecticleFound){
                             // Invader particle effect upon destruction
                             createParticles({
-                                object: invader
+                                object: invader,
+                                fades: true
                             })
                             // remove invader and projectile upon collision
                             grid.invaders.splice(i, 1)
