442cc4c913a43bafe5155a103a48041ed0f92098
Jambr0li <jasonkovalenko@gmail.com>
2023-10-14 11:39:47 -0700

    Grids of invaders spawn at regular intervals

--- index.js
+++ index.js
@@ -153,7 +153,7 @@ class Grid{
 
 const player = new Player()
 const projectiles = []
-const grids = [new Grid()]
+const grids = []
 const keys = {
     a: {
         pressed: false
@@ -165,6 +165,10 @@ const keys = {
         pressed: false
     }
 }
+
+let frames = 0
+let randomInterval = Math.floor((Math.random() * 500) + 500)
+
 
 function animate(){
     requestAnimationFrame(animate)
@@ -198,6 +202,17 @@ function animate(){
         player.velocity.x = 0;
         player.rotation = 0
     }
+
+    // console.log(frames);
+    
+    if (frames % randomInterval === 0) {
+        grids.push(new Grid())
+        randomInterval = Math.floor((Math.random() * 500) + 500)
+        frames = 0;
+        console.log(randomInterval);
+    }
+
+    frames++
 }
 
 animate()
